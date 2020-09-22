00bb1443b83aa9cd3e50128e7f4ec6347d574e92
scenaire <tempesta-psyzeoul@hotmail.com>
2020-09-22 22:44:25 +0700

    edit env

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config
+require('dotenv').config();
 
 const req = require('request');
 var TwitterBot = require("node-twitterbot").TwitterBot;
 const { ICONS } = require('./icons.js');
 
 var Bot = new TwitterBot({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
 var random = Math.floor(Math.random() * 8);
