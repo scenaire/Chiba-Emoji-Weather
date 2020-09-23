bff471ab5eb11a456d01e3e0f26293c71407ea37
scenaire <tempesta-psyzeoul@hotmail.com>
2020-09-24 00:30:07 +0700

    trying to kill myself

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req,res) {
+    res.sendFile('index.html', {root: __dirname})
+});
+
+app.get('/callback', function(req, res) {
     res.send('hello world');
 });
 
